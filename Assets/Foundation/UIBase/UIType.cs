d90c02bfaa43bc23b0509d217fc007cc57ae289e
杨万盛 <1748896856@qq.com>
2018-08-14 08:56:59 +0800

    Initial commit

---
+++ Assets/Foundation/UIBase/UIType.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ *	
+ *  Define View's Path And Name
+ *
+ *	by Xuanyi
+ *
+ */
+
+namespace MoleMole
+{
+	public class UIType {
+
+        /// <summary>
+        ///View 的路径
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// View 的名称
+        /// </summary>
+        public string Name { get; private set; }
+
+        public UIType(string path)
+        {
+            Path = path;
+            Name = path.Substring(path.LastIndexOf('/') + 1);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("path : {0} name : {1}", Path, Name);
+        }
+ 
+
+        /// <summary>
+        /// 将指定名称的 界面放在UItype  中
+        /// </summary>
+        public static readonly UIType MainMenu = new UIType("View/MainMenuView");
+        public static readonly UIType OptionMenu = new UIType("View/OptionMenuView");
+        public static readonly UIType NextMenu = new UIType("View/NextMenuView");
+        public static readonly UIType HighScore = new UIType("View/HighScoreView");
+    }
+}
---
+++ Assets/Script/HighScoreView.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ *	
+ *  
+ *
+ *	by Xuanyi
+ *
+ */
+
+namespace MoleMole
+{
+   /// <summary>
+   ///  代指游戏中每个View的上下文,存储了这个界面的各种数据状态
+   ///  游戏中会通过 栈的方式管理Context  
+   /// </summary>
+    public class HighScoreContext : BaseContext
+    {
+        public HighScoreContext()
+            : base(UIType.HighScore)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 代指中的主要界面, 主界面，装备界面，装备详情界面
+    /// 在 View  包含了界面处理数据的逻辑
+    /// </summary>
+    public class HighScoreView : AnimateView
+    {
+        public GridScroller _gridScroller;
+
+        public override void OnEnter(BaseContext context)
+        {
+            base.OnEnter(context);
+            _gridScroller.Init(OnChange, 100, new Vector2(0.12f, 1f));
+        }
+
+        public override void OnExit(BaseContext context)
+        {
+            base.OnExit(context);
+        }
+
+        public override void OnPause(BaseContext context)
+        {
+            base.OnPause(context);
+        }
+
+        public override void OnResume(BaseContext context)
+        {
+            base.OnResume(context);
+        }
+
+        public void BackCallBack()
+        {
+            Singleton<ContextManager>.Instance.Pop();
+        }
+
+        public void OnChange(Transform trans, int index)
+        {
+            trans.GetComponent<HighScoreItem>().Init(index);
+        }
+    }
+}
